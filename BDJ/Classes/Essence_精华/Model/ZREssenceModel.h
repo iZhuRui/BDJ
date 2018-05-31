80a9f2ca30fe87efe0824cdda5087ca7b39305ea
zhurui <www.515246872@qq.com>
2018-06-01 00:46:21 +0800

    test

---
+++ BDJ/Classes/Essence_精华/Model/ZREssenceModel.h
@@ -0,0 +1,13 @@
+//
+//  ZREssenceModel.h
+//  BDJ
+//
+//  Created by 朱锐 on 2018/6/1.
+//  Copyright © 2018年 edz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZREssenceModel : NSObject
+
+@end
---
+++ BDJ/Classes/Essence_精华/Model/ZREssenceModel.m
@@ -0,0 +1,13 @@
+//
+//  ZREssenceModel.m
+//  BDJ
+//
+//  Created by 朱锐 on 2018/6/1.
+//  Copyright © 2018年 edz. All rights reserved.
+//
+
+#import "ZREssenceModel.h"
+
+@implementation ZREssenceModel
+
+@end
